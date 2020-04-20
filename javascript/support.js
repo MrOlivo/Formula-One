f1399d6547dbc06fbe687d453003986c39b50f58
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-04-20 11:17:06 -0600

    Create support.js

---
+++ javascript/support.js
@@ -0,0 +1,24 @@
+function chargeSeasons() {
+    fetch('/json/season_list.json')
+        .then(response => response.json())
+        .then(data => {
+            const data_list = document.getElementById('seasons');
+            const season_list = data.MRData.SeasonTable.Seasons;
+            season_list.forEach(element => {
+                let option = document.createElement('option');
+                option.value = element.season;
+                data_list.appendChild(option);
+            });
+        })
+        .catch(err => {
+
+        })
+}
+
+document.getElementsByName('season-search')[0].addEventListener("keyup", (event) => {
+    let val = document.getElementsByName('season-search')[0].value;
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        seasonResults(val);
+    }
+});
