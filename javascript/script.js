a04844a598b87f594bc46715abad080fb0091216
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-06-11 08:27:41 -0600

    Update script.js
    Async & Await

--- javascript/script.js
+++ javascript/script.js
@@ -1,72 +1,71 @@
-function seasonResults(season) {
 
+const seasonResults = async (season) => {
     if (season.length < 4) {
         return
     }
 
     const URL = `https://ergast.com/api/f1/${season}/results.json?limit=500&offset=0`
 
-    //Arrays -> forEach
-    //Dictionary u Objetos -> punto o corchetes
+    try {
+        const res = await fetch(URL)
+        const data = await res.json()
 
-    fetch(URL)
-        .then(response => response.json())
-        .then(season => {
-            const app = document.getElementById('app')
-            app.innerHTML = ""
-            const race_table = season.MRData.RaceTable
+        const app = document.getElementById('app')
+        app.innerHTML = ""
 
-            race_table.Races.forEach(race => {
-                //  Añada la badera del pais como fondo
-                const race_head = createCustomElements('', ['title'])
-                const country = race.Circuit.Location.country.split(' ').join('_')
-                race_head.style.backgroundImage = `url(flags/${country.toLowerCase()}.svg)`
+        const {RaceTable} = data.MRData
 
-                //  Añade el Round
-                race_head.appendChild(createCustomElements(race.round, ['square', 'square__black']))
+        RaceTable.Races.forEach(race => {
+            //  Añada la badera del pais como fondo
+            const race_head = createCustomElements('', ['title'])
+            const country = race.Circuit.Location.country.split(' ').join('_')
+            race_head.style.backgroundImage = `url(flags/${country.toLowerCase()}.svg)`
 
-                //  Crea la tabla que contiene los resultados del GP
-                const table = document.createElement('table')
-                const thead = document.createElement('thead')
-                thead.innerHTML = '<tr> <th>Pos</th> <th>Driver</th> <th>Constructor</th> <th>Points</th> </tr>'
-                const tbody = document.createElement('tbody')
+            //  Añade el Round
+            race_head.appendChild(createCustomElements(race.round, ['square', 'square__black']))
 
-                //  Bucle de Resultados (race.Results) de cada Carrera (race) por 
-                race.Results.forEach(result => {
-                    //  Llenamos la tabla con los resultados de cada piloto
-                    let tr = document.createElement('tr')
-                    tr.innerHTML = `<td>${result.position}</td>
+            //  Crea la tabla que contiene los resultados del GP
+            const table = document.createElement('table')
+            const thead = document.createElement('thead')
+            thead.innerHTML = '<tr> <th>Pos</th> <th>Driver</th> <th>Constructor</th> <th>Points</th> </tr>'
+            const tbody = document.createElement('tbody')
+
+            //  Bucle de Resultados (race.Results) de cada Carrera (race) por 
+            race.Results.forEach(result => {
+                //  Llenamos la tabla con los resultados de cada piloto
+                let tr = document.createElement('tr')
+                tr.innerHTML = `<td>${result.position}</td>
                                 <td>${result.Driver.givenName} ${result.Driver.familyName.toUpperCase()}</td>
                                 <td>${result.Constructor.name}</td>
                                 <td>${result.points}</td>`
 
-                    tbody.appendChild(tr)
-                })
-
-                //  Llenamos la tabla con thead & tbody
-                table.appendChild(thead)
-                table.appendChild(tbody)
-
-                //  Agrega el nombre le GP y la tabla con los resulatdos de la carrera
-                const item = createCustomElements('', ['item'])
-
-                //  Añadimos info como el nombre de la carrera y el circuito
-                const gp = createCustomElements('', ['gp'])
-                gp.appendChild(createCustomElements(race.raceName, ['race']))
-                gp.appendChild(createCustomElements(race.Circuit.circuitName, ['circuit']))
-
-                //  Agregamos a item los elementos que lo componen
-                item.appendChild(race_head)
-                item.appendChild(gp)
-                item.appendChild(table)
-
-                //  Añadimos a APP el elemento item
-                app.appendChild(item)
-
+                tbody.appendChild(tr)
             })
 
+            //  Llenamos la tabla con thead & tbody
+            table.appendChild(thead)
+            table.appendChild(tbody)
+
+            //  Agrega el nombre le GP y la tabla con los resultados de la carrera
+            const item = createCustomElements('', ['item'])
+
+            //  Añadimos info como el nombre de la carrera y el circuito
+            const gp = createCustomElements('', ['gp'])
+            gp.appendChild(createCustomElements(race.raceName, ['race']))
+            gp.appendChild(createCustomElements(race.Circuit.circuitName, ['circuit']))
+
+            //  Agregamos a item los elementos que lo componen
+            item.appendChild(race_head)
+            item.appendChild(gp)
+            item.appendChild(table)
+
+            //  Añadimos a APP el elemento item
+            app.appendChild(item)
+
         })
-        .catch(err => console.log(err))
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 //  Función que crea "elements" con:
