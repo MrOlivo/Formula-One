501a878a4248f7265c68e9c4d3f1641e13dc06a7
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-06-11 08:18:51 -0600

    Update support.js
    Async & Await

--- javascript/support.js
+++ javascript/support.js
@@ -1,25 +1,28 @@
-function chargeSeasons() {
-    const season_list = 'https://ergast.com/api/f1/seasons.json?limit=100';
-    fetch(season_list)
-        .then(response => response.json())
-        .then(data => {
-            const data_list = document.getElementById('seasons');
-            const season_list = data.MRData.SeasonTable.Seasons;
-            season_list.forEach(element => {
-                let option = document.createElement('option');
-                option.value = element.season;
-                data_list.appendChild(option);
-            });
-        })
-        .catch(err => {
+const chargeSeasons = async () => {
+    const season_url = 'https://ergast.com/api/f1/seasons.json?limit=100'
+    try {
+        const res = await fetch(season_url)
+        const data = await res.json()
 
+        const data_list = document.getElementById('seasons')
+        const {
+            Seasons
+        } = data.MRData.SeasonTable
+
+        Seasons.forEach(element => {
+            let option = document.createElement('option')
+            option.value = element.season
+            data_list.appendChild(option)
         })
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 document.getElementsByName('season-search')[0].addEventListener("keypress", (event) => {
-    let val = document.getElementsByName('season-search')[0].value;
+    let val = document.getElementsByName('season-search')[0].value
     if (event.keyCode === 13) {
-        event.preventDefault();
-        seasonResults(val);
+        event.preventDefault()
+        seasonResults(val)
     }
-});
+})
