37a96f5278eca09ccf6a1c65d7101ab93d63a14a
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-07-02 20:11:00 -0600

    Prettierise

--- javascript/script.js
+++ javascript/script.js
@@ -1,83 +1,88 @@
-
 const seasonResults = async (season) => {
     if (season.length < 4) {
-        return
+        return;
     }
 
-    const URL = `https://ergast.com/api/f1/${season}/results.json?limit=500&offset=0`
+    const URL = `https://ergast.com/api/f1/${season}/results.json?limit=500&offset=0`;
 
     try {
-        const res = await fetch(URL)
-        const data = await res.json()
+        const res = await fetch(URL);
+        const data = await res.json();
 
-        const app = document.getElementById('app')
-        app.innerHTML = ""
+        const app = document.getElementById('app');
+        app.innerHTML = '';
 
-        const {RaceTable} = data.MRData
+        const { RaceTable } = data.MRData;
 
-        RaceTable.Races.forEach(race => {
+        RaceTable.Races.forEach((race) => {
             //  Añada la bandera del pais como fondo
-            const race_head = createCustomElements('', ['title'])
-            const country = race.Circuit.Location.country.split(' ').join('_')
-            race_head.style.backgroundImage = `url(flags/${country.toLowerCase()}.svg)`
+            const race_head = createCustomElements('', ['title']);
+            const country = race.Circuit.Location.country.split(' ').join('_');
+            race_head.style.backgroundImage = `url(flags/${country.toLowerCase()}.svg)`;
 
             //  Añade el Round
-            race_head.appendChild(createCustomElements(race.round, ['square', 'square__black']))
+            race_head.appendChild(
+                createCustomElements(race.round, ['square', 'square__black'])
+            );
 
             //  Crea la tabla que contiene los resultados del GP
-            const table = document.createElement('table')
-            const thead = document.createElement('thead')
-            thead.innerHTML = '<tr> <th>Pos</th> <th>Driver</th> <th>Constructor</th> <th>Points</th> </tr>'
-            const tbody = document.createElement('tbody')
+            const table = document.createElement('table');
+            const thead = document.createElement('thead');
+            thead.innerHTML =
+                '<tr> <th>Pos</th> <th>Driver</th> <th>Constructor</th> <th>Points</th> </tr>';
+            const tbody = document.createElement('tbody');
 
-            //  Bucle de Resultados (race.Results) de cada Carrera (race) por 
-            race.Results.forEach(result => {
+            //  Bucle de Resultados (race.Results) de cada Carrera (race) por
+            race.Results.forEach((result) => {
                 //  Llenamos la tabla con los resultados de cada piloto
-                let tr = document.createElement('tr')
+                let tr = document.createElement('tr');
                 tr.innerHTML = `<td>${result.position}</td>
-                                <td>${result.Driver.givenName} ${result.Driver.familyName.toUpperCase()}</td>
+                                <td>${
+                                    result.Driver.givenName
+                                } ${result.Driver.familyName.toUpperCase()}</td>
                                 <td>${result.Constructor.name}</td>
-                                <td>${result.points}</td>`
+                                <td>${result.points}</td>`;
 
-                tbody.appendChild(tr)
-            })
+                tbody.appendChild(tr);
+            });
 
             //  Llenamos la tabla con thead & tbody
-            table.appendChild(thead)
-            table.appendChild(tbody)
+            table.appendChild(thead);
+            table.appendChild(tbody);
 
             //  Agrega el nombre le GP y la tabla con los resultados de la carrera
-            const item = createCustomElements('', ['item'])
+            const item = createCustomElements('', ['item']);
 
             //  Añadimos info como el nombre de la carrera y el circuito
-            const gp = createCustomElements('', ['gp'])
-            gp.appendChild(createCustomElements(race.raceName, ['race']))
-            gp.appendChild(createCustomElements(race.Circuit.circuitName, ['circuit']))
+            const gp = createCustomElements('', ['gp']);
+            gp.appendChild(createCustomElements(race.raceName, ['race']));
+            gp.appendChild(
+                createCustomElements(race.Circuit.circuitName, ['circuit'])
+            );
 
             //  Agregamos a item los elementos que lo componen
-            item.appendChild(race_head)
-            item.appendChild(gp)
-            item.appendChild(table)
+            item.appendChild(race_head);
+            item.appendChild(gp);
+            item.appendChild(table);
 
             //  Añadimos a APP el elemento item
-            app.appendChild(item)
-
-        })
+            app.appendChild(item);
+        });
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 //  Función que crea "elements" con:
 //      Contenido que le proporcionemos
 //      Clases que pasemos como parametro
 function createCustomElements(contenido, clases = []) {
-    let element = document.createElement('div')
-    element.textContent = contenido
+    let element = document.createElement('div');
+    element.textContent = contenido;
 
-    clases.forEach(clase => {
-        element.classList.add(clase)
-    })
+    clases.forEach((clase) => {
+        element.classList.add(clase);
+    });
 
-    return element
+    return element;
 }
--- javascript/support.js
+++ javascript/support.js
@@ -1,28 +1,28 @@
 const chargeSeasons = async () => {
-    const season_url = 'https://ergast.com/api/f1/seasons.json?limit=100'
+    const season_url = 'https://ergast.com/api/f1/seasons.json?limit=100';
     try {
-        const res = await fetch(season_url)
-        const data = await res.json()
+        const res = await fetch(season_url);
+        const data = await res.json();
 
-        const data_list = document.getElementById('seasons')
-        const {
-            Seasons
-        } = data.MRData.SeasonTable
+        const data_list = document.getElementById('seasons');
+        const { Seasons } = data.MRData.SeasonTable;
 
-        Seasons.forEach(element => {
-            let option = document.createElement('option')
-            option.value = element.season
-            data_list.appendChild(option)
-        })
+        Seasons.forEach((element) => {
+            let option = document.createElement('option');
+            option.value = element.season;
+            data_list.appendChild(option);
+        });
     } catch (error) {
         console.log(error);
     }
-}
+};
 
-document.getElementsByName('season-search')[0].addEventListener("keypress", (event) => {
-    let val = document.getElementsByName('season-search')[0].value
-    if (event.keyCode === 13) {
-        event.preventDefault()
-        seasonResults(val)
-    }
-})
+document
+    .getElementsByName('season-search')[0]
+    .addEventListener('keypress', (event) => {
+        let val = document.getElementsByName('season-search')[0].value;
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            seasonResults(val);
+        }
+    });
